00132ca4f6cf967b0d319c845566385097c5bece
Matheusl-Silva <matleandrosilva@gmail.com>
2026-02-17 19:19:07 -0300

    ✨ feat: Criação do footer e header
    Criado o footer e Header da página principal, também foi feito ajustes na página de notfound para ficar visualmente mais bonito. Criado a rota de cadastro de paróquias.

---
+++ ecclesiafront/app/cadastroParoquia/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function CadastroParoquia() {
+  return (
+    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+      página de cadastro da paróquia
+    </p>
+  );
+}
